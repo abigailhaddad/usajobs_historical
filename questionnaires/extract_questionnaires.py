2f35edeec8c5471fe7ff122fb1c6f5fbba09b289
abigailhaddad <abigail.haddad@gmail.com>
2025-09-11 16:28:31 -0400

    fixed

--- questionnaires/extract_questionnaires.py
+++ questionnaires/extract_questionnaires.py
@@ -135,7 +135,7 @@ def extract_questionnaire_links_from_job(job_row):
         if match not in links:
             links.append(match)
     
-    # Look for Monster Government questionnaire links
+    # Check for Monster Government questionnaire links (but don't add them)
     monster_patterns = [
         r'https://jobs\.monstergovt\.com/[^/]+/vacancy/previewVacancyQuestions\.hms\?[^"\'\s<>]+',
         r'https://jobs\.monstergovt\.com/[^/]+/ros/rosDashboard\.hms\?[^"\'\s<>]+'
@@ -143,10 +143,9 @@ def extract_questionnaire_links_from_job(job_row):
     
     for pattern in monster_patterns:
         monster_matches = re.findall(pattern, job_str)
-        for match in monster_matches:
-            if match not in links:
-                links.append(match)
-                has_monster_link = True
+        if monster_matches:
+            has_monster_link = True
+            break
     
     return links, occupation_series, occupation_name, position_location, grade_code, position_schedule, service_type, low_grade, high_grade, has_monster_link
 
